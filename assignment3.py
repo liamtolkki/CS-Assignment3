623076b8dd731653c7c09067c79bc72af9176aad
Liam Tolkkinen <liamtolkki@gmail.com>
2024-04-05 17:37:56 -0400

    added a 2D array that will hold the scores as the function progresses

--- assignment3.py
+++ assignment3.py
@@ -1,10 +1,12 @@
 #Assignment 3
+#String Matching
 #Group: Liam Tolkkinen
 #due 04/11/2024
 
 import re
 import sys
-
+def create_2d_array(rows, cols):
+    return [[0 for _ in range(cols)] for _ in range(rows)]
 # Returns a number found in the filename
 def getNumber(filename):
     digits = re.findall(r'\d+', filename) # Regex to find the digits in the name
@@ -26,14 +28,17 @@ def main():
     insertDeleteCost = None
     matchCost = None
     mismatchCost = None
-
     with open(inputPath, "r") as infile:
-        string1 = infile.readline()
-        string2 = infile.readline()
+        string1 = infile.readline().strip()
+        string2 = infile.readline().strip()
         insertDeleteCost = int(infile.readline().strip())
         matchCost = int(infile.readline().strip())
         mismatchCost = int(infile.readline().strip())
         infile.close()
+    #testing purposes:
+    #print("String1: \"" + string1 + "\"\n" + "String2: \"" + string2 + "\"\n")
 
+    #stringArray will hold the costs as it goes (2D array)
+    stringArray = create_2d_array(len(string1), len(string2))
 if __name__ == "__main__":
     main()
