8bb50ef424930e55f89a99d163332321b4f5f1fb
Liam Tolkkinen <liamtolkki@gmail.com>
2024-04-10 18:58:10 -0400

    the costMatrix is functional
    next: implement the matrix traversal

--- assignment3.py
+++ assignment3.py
@@ -29,6 +29,15 @@ def printMatrix():
                 
         print() #newline
 
+#returns the cost of diagonal traversal
+def alpha(i, j):
+    if string1[i] == string2[j]:
+        return matchCost
+    else:
+        return mismatchCost
+
+
+
 def main():
     if len(sys.argv) != 2:
         print("make sure to include the file name of the input file!\n")
@@ -36,12 +45,16 @@ def main():
     inputFileName = sys.argv[1] # argv[1] contains the second argument (filename)
     fileNumber = getNumber(inputFileName)
     inputPath = "testing/" + inputFileName
-    #initialize all values to be null
-    string1 = None
-    string2 = None
-    insertDeleteCost = None
-    matchCost = None
-    mismatchCost = None
+    #declare all global variables
+    global string1
+    global string2
+    global insertDeleteCost
+    global matchCost
+    global mismatchCost
+    global nRows
+    global nCols
+    global costMatrix 
+
     with open(inputPath, "r") as infile:
         string1 = infile.readline().strip()
         string2 = infile.readline().strip()
@@ -49,13 +62,10 @@ def main():
         matchCost = int(infile.readline().strip())
         mismatchCost = int(infile.readline().strip())
         infile.close()
-    #declare global variables
-    global nRows
+    #initialize values
     nRows = len(string1)
-    global nCols
     nCols = len(string2)
     #costMatrix will hold the costs as it goes (2D array)
-    global costMatrix 
     costMatrix = create_2d_array(nRows, nCols)
     #initialize the first row and column with gapCost * i O(M + N):
     for i in range(nCols):
@@ -65,9 +75,19 @@ def main():
     print("String1: \"" + string1 + "\"")
     print("String2: \"" + string2 + "\"")
 
+    #calculate the cell costs:
+    #start with indexes at 1 since first row and column already filled out!
+    for i in range(1, nRows):
+        for j in range(1, nCols):
+            diagonalCost = alpha(i, j) + costMatrix[i - 1][j - 1]
+            gapDown = insertDeleteCost + costMatrix[i - 1][j]
+            gapRight = insertDeleteCost + costMatrix[i][j - 1]
+            #minimum cost becomes the cell
+            costMatrix[i][j] = min(diagonalCost, gapDown, gapRight)
+
+
     printMatrix() #testing purposes
 
-    #calculate the cell costs:
 
     
 if __name__ == "__main__":
